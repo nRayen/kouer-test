a2e3135c57532bf97492d49fe55708427cde65d8
nRayen <rayen.ndiaye@gmail.com>
2025-07-12 16:09:33 +0200

    (main) : homepage to link to the signup page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,9 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold">Hello World</h1>
-      <Link href="/signup">Signup</Link>
+      <h1 className="text-4xl font-bold">Rayen NDIAYE</h1>
+      <h2 className="text-2xl font-medium">Test Technique alternant développeur web chez Kouer</h2>
+      <Link href="/signup" className="text-blue-500">Lien vers le formulaire de création de compte</Link>
     </div>
   );
 }
